c698b56b0db2213d0217fcaba9b791ffb4f9ff61
qzhang16 <qzhang16@ra.rockwell.com>
2021-08-03 21:51:50 +0800

    4th load

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,6 @@
 // const name01 = require('./utils.js');
 //const name = 'Qiang';
 // console.log(name01(1,2));
+const validator = require('validator');
 const notes = require('./notes.js');
 console.log(notes());
--- notes-app/utils.js
+++
@@ -1,8 +0,0 @@
-console.log('utils.js');
-
-const add = function(a,b) {
-    return a+b;
-};
-
-const name = 'Mike';
-module.exports = add;
