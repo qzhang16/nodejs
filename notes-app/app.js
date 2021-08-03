28dd8f2839747bb7ec05c8e8dc115db30970fe8e
qzhang16 <qzhang16@ra.rockwell.com>
2021-08-03 19:58:04 +0800

    3rd load

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,5 @@
-const fs1 = require("fs");
-
-//fs1.writeFileSync('notes.txt','This file was created by Node.js1 !');
-fs1.appendFileSync('notes.txt','my name is Qiang !');
-
-console.log(fs1.readFileSync('notes.txt'));
+// const name01 = require('./utils.js');
+//const name = 'Qiang';
+// console.log(name01(1,2));
+const notes = require('./notes.js');
+console.log(notes());
