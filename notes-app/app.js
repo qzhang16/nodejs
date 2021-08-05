ee5e52e74903ef4e903bd7e1acdb5c7ee314d118
qzhang16 <qzhang16@ra.rockwell.com>
2021-08-05 20:39:52 +0800

    fifth load

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,14 @@
 // const name01 = require('./utils.js');
 //const name = 'Qiang';
 // console.log(name01(1,2));
-const validator = require('validator');
+// const validator = require('validator');
+const chalk = require('chalk');
 const notes = require('./notes.js');
-console.log(notes());
+//console.log(notes());
+// console.log(validator.isEmail('abcra.rockwell.com'));
+//console.log(chalk.blue.bold.inverse('success !'));
+//console.log(process.argv[2]);
+const command = process.argv[2];
+if (command === 'add') {
+    console.log('Adding a note !');
+}
