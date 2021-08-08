9bf43ffd8efe3c180397d3d393c4cfc9d485df11
qzhang16 <qzhang16@ra.rockwell.com>
2021-08-08 15:21:32 +0800

    6th try

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,11 +4,71 @@
 // const validator = require('validator');
 const chalk = require('chalk');
 const notes = require('./notes.js');
+const yargs = require('yargs');
+yargs.version('1.1.0');
 //console.log(notes());
 // console.log(validator.isEmail('abcra.rockwell.com'));
 //console.log(chalk.blue.bold.inverse('success !'));
 //console.log(process.argv[2]);
-const command = process.argv[2];
-if (command === 'add') {
-    console.log('Adding a note !');
-}
+// const command = process.argv[2];
+// if (command === 'add') {
+//     console.log('Adding a note !');
+// } else if (command === 'remove') {
+//     console.log('removing a note !');
+// }
+// console.log(process.argv);
+//console.log(yargs.argv);
+
+//add
+yargs.command({
+    command: 'add',
+    describe: 'Adding a new note',
+    builder: {
+        title: {
+            decribe: 'title the new note',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'content of the new note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        // console.log('Title: ',argv.title);
+        // console.log('Body: ',argv.body);
+        notes.addNote(argv.title,argv.body);
+    }
+});
+
+//remove
+yargs.command({
+    command: 'remove',
+    describe: 'Removing the note',
+    handler: function () {
+        console.log('Removing the note !');
+    }
+});
+
+//list
+yargs.command({
+    command: 'list',
+    describe: 'Listing the notes',
+    handler: function () {
+        console.log('Listing the notes !');
+    }
+});
+
+//read
+yargs.command({
+    command: 'read',
+    describe: 'reading the note',
+    handler: function () {
+        console.log('reading the note !');
+    }
+});
+
+yargs.parse();
+
+// console.log(yargs.argv);
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,4 +1,25 @@
+const fs = require('fs');
+
 const getNotes = function() {
     return "Your notes...";
 };
-module.exports = getNotes;
+const addNote = function(title,body) {
+    const notes = loadNotes();
+    console.log(notes);
+
+};
+
+const loadNotes = function(){
+    try{
+        return JSON.parse(fs.readFileSync('notes.json').toString());
+        
+    }catch (e) {
+        return [];
+    }
+    //const data = JSON.parse(fs.readFileSync('notes.json').toString());
+};
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+};
